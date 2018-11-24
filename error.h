bd3464444d13469efbc6df0e6c8511d0cf451def
yuhao0603 <me.yuhao@outlook.com>
2018-11-24 20:46:05 +0800

    添加错误返回值宏

---
+++ error.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: error.h
+	> Author: yuhao
+	> Mail: me.yuhao@outlook.com
+	> Created Time: 2018年11月24日 星期六 20时34分41秒
+ ************************************************************************/
+
+#ifndef _ERROR_H
+#define _ERROR_H
+
+#define ERR_SUCCESS 0
+#define ERR_FAILED  1
+
+#endif
