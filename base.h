4dc2992b34fdfc24ff5824725f43fa1e4fa253eb
yuhao0603 <me.yuhao@outlook.com>
2018-12-02 14:42:29 +0800

    add base.h

---
+++ base.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: base.h
+	> Author: yuhao
+	> Mail: me.yuhao@outlook.com
+	> Created Time: 2018年12月02日 星期日 14时35分52秒
+ ************************************************************************/
+
+#ifndef _BASE_H
+#define _BASE_H
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+#endif
